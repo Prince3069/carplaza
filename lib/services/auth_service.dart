40ae38836aa536b7016ce32b62c97db0264b99ee
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-27 20:26:09 +0100

    editted the auth service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,6 @@
 // File: lib/services/auth_service.dart
 import 'package:car_plaza/models/car.dart';
+import 'package:car_plaza/models/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
