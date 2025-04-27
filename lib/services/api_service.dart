cddb028d238d8d475a12cdafd01adfcb5c411db2
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-27 20:26:45 +0100

    commented out the api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,22 +1,22 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import '../models/car.dart';
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import '../models/car.dart';
 
-class ApiService {
-  final String apiKey = "8bbb3c78179ada9cd0e955c5f88a55dc";
+// class ApiService {
+//   final String apiKey = "8bbb3c78179ada9cd0e955c5f88a55dc";
 
-  Future<List<Car>> fetchCars() async {
-    final url =
-        Uri.parse("https://api.auto-data.net/v1/car-list?api_key=$apiKey");
-    final response = await http.get(url);
+//   Future<List<Car>> fetchCars() async {
+//     final url =
+//         Uri.parse("https://api.auto-data.net/v1/car-list?api_key=$apiKey");
+//     final response = await http.get(url);
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final List carsList = data['cars'];
+//     if (response.statusCode == 200) {
+//       final data = json.decode(response.body);
+//       final List carsList = data['cars'];
 
-      return carsList.map((json) => Car.fromApi(json)).toList();
-    } else {
-      throw Exception("Failed to fetch cars from API");
-    }
-  }
-}
+//       return carsList.map((json) => Car.fromApi(json)).toList();
+//     } else {
+//       throw Exception("Failed to fetch cars from API");
+//     }
+//   }
+// }
