666983c81c760ee3cc1fe7c8f147147e28d9afd8
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-27 20:33:58 +0100

    added a new working api serviice

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -50,3 +50,41 @@
 //     }
 //   }
 // }
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/car.dart';
+
+class ApiService {
+  final String apiKey =
+      "XqskErMyTAV7o67n+CT54w==I4pRbsFxjwTNPaX5"; // ✅ Your API Key
+
+  Future<List<Car>> fetchCars() async {
+    final url = Uri.parse("https://api.api-ninjas.com/v1/cars?limit=10");
+
+    final response = await http.get(
+      url,
+      headers: {
+        "X-Api-Key": apiKey,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List data = json.decode(response.body);
+      return data
+          .map((json) => Car(
+                id: json['model'].toString(),
+                title: "${json['make']} ${json['model']}",
+                imageUrl:
+                    "https://source.unsplash.com/600x400/?${json['make']}+${json['model']}", // smart random image
+                price: 20000, // fake price for now
+                location: "Unknown",
+                isFromFirebase: false,
+              ))
+          .toList();
+    } else {
+      print('Error: ${response.body}');
+      throw Exception("Failed to fetch cars from API");
+    }
+  }
+}
