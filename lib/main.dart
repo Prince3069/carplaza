7252c57b3cfda3afd4909a382e765e0eff19a05b
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-28 10:45:25 +0100

    editted the splash screen and now there are no more errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,142 @@
-import 'package:car_plaza/screens/auth/login_screen.dart';
-import 'package:car_plaza/screens/home/home_screen.dart';
-import 'package:car_plaza/screens/profile/profile_screen.dart';
-import 'package:car_plaza/screens/sell/sell_screen.dart';
-import 'package:car_plaza/chat/chat_screen.dart';
-import 'package:car_plaza/splash/splash_screen.dart';
-import 'package:car_plaza/firebase_options.dart';
+// import 'package:car_plaza/screens/auth/login_screen.dart';
+// import 'package:car_plaza/screens/home/home_screen.dart';
+// import 'package:car_plaza/screens/profile/profile_screen.dart';
+// import 'package:car_plaza/screens/sell/sell_screen.dart';
+// import 'package:car_plaza/chat/chat_screen.dart';
+// import 'package:car_plaza/splash/splash_screen.dart';
+// import 'package:car_plaza/firebase_options.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:provider/provider.dart';
+// import 'auth/auth_provider.dart'; // We will create this file too
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(const CarPlazApp());
+// }
+
+// class CarPlazApp extends StatelessWidget {
+//   const CarPlazApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(create: (_) => AuthProvider()),
+//       ],
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: 'CarPlaz',
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//           fontFamily: 'Poppins',
+//         ),
+//         home: const SplashScreen(),
+//       ),
+//     );
+//   }
+// }
+
+// class BottomNavScreen extends StatefulWidget {
+//   const BottomNavScreen({super.key});
+
+//   @override
+//   State<BottomNavScreen> createState() => _BottomNavScreenState();
+// }
+
+// class _BottomNavScreenState extends State<BottomNavScreen> {
+//   int _selectedIndex = 0;
+
+//   static final List<Widget> _screens = [
+//     HomeScreen(),
+//     ChatScreen(),
+//     SellCarScreen(),
+//     ChatScreen(),
+//     ProfileScreen(),
+//   ];
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+
+//   final List<BottomNavigationBarItem> _bottomItems = const [
+//     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+//     BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+//     BottomNavigationBarItem(icon: Icon(Icons.add_circle), label: 'Sell'),
+//     BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
+//     BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _screens[_selectedIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: _bottomItems,
+//         currentIndex: _selectedIndex,
+//         selectedItemColor: Colors.blueAccent,
+//         unselectedItemColor: Colors.grey,
+//         onTap: _onItemTapped,
+//         type: BottomNavigationBarType.fixed,
+//       ),
+//     );
+//   }
+// }
+
+// =================== lib/main.dart (updated for light/dark mode) ===================
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'auth/auth_provider.dart'; // We will create this file too
+import 'services/auth_service.dart';
+import 'screens/onboarding/onboarding_screen.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/auth/register_screen.dart';
+import 'screens/home/home_screen.dart';
+import 'screens/search_screen.dart';
+import 'screens/sell/sell_screen.dart';
+import 'screens/messages/messages_screen.dart';
+import 'screens/profile/profile_screen.dart';
+import 'routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const CarPlazApp());
 }
 
 class CarPlazApp extends StatelessWidget {
-  const CarPlazApp({super.key});
+  const CarPlazApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-      ],
+    return ChangeNotifierProvider(
+      create: (_) => AuthService(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'CarPlaz',
         theme: ThemeData(
+          brightness: Brightness.light,
           primarySwatch: Colors.blue,
-          fontFamily: 'Poppins',
+          scaffoldBackgroundColor: Colors.white,
         ),
-        home: const SplashScreen(),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.black,
+        ),
+        themeMode:
+            ThemeMode.system, // This auto switches based on device setting
+        initialRoute: '/',
+        routes: appRoutes,
       ),
     );
   }
 }
 
-class BottomNavScreen extends StatefulWidget {
-  const BottomNavScreen({super.key});
-
-  @override
-  State<BottomNavScreen> createState() => _BottomNavScreenState();
-}
-
-class _BottomNavScreenState extends State<BottomNavScreen> {
-  int _selectedIndex = 0;
-
-  static final List<Widget> _screens = [
-    HomeScreen(),
-    ChatScreen(),
-    SellCarScreen(),
-    ChatScreen(),
-    ProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  final List<BottomNavigationBarItem> _bottomItems = const [
-    BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-    BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-    BottomNavigationBarItem(icon: Icon(Icons.add_circle), label: 'Sell'),
-    BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
-    BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: _bottomItems,
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blueAccent,
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
-      ),
-    );
-  }
-}
+// =============================================================
--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -1,53 +1,110 @@
-import 'package:car_plaza/screens/auth/login_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:car_plaza/main.dart'; // To navigate to BottomNav after login
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:car_plaza/screens/auth/login_screen.dart';
+// import 'package:car_plaza/widgets/bottom_nav_bar.dart';
+// import 'package:flutter/material.dart';
+// import 'package:car_plaza/main.dart'; // To navigate to BottomNav after login
+// import 'package:firebase_auth/firebase_auth.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
+
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     _navigate();
+//   }
+
+//   void _navigate() async {
+//     await Future.delayed(const Duration(seconds: 3));
+
+//     User? user = FirebaseAuth.instance.currentUser;
+
+//     if (user != null) {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (context) => const BottomNavBar()),
+//       );
+//     } else {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (context) => const LoginScreen()),
+//       );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Image.asset('assets/images/logo.png', height: 120),
+//             const SizedBox(height: 20),
+//             const CircularProgressIndicator(color: Colors.blueAccent),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const BottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  _BottomNavBarState createState() => _BottomNavBarState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _navigate();
-  }
-
-  void _navigate() async {
-    await Future.delayed(const Duration(seconds: 3));
-
-    User? user = FirebaseAuth.instance.currentUser;
-
-    if (user != null) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const BottomNavScreen()),
-      );
-    } else {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
-      );
-    }
-  }
-
+class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('CarPlaz'),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/logo.png', height: 120),
-            const SizedBox(height: 20),
-            const CircularProgressIndicator(color: Colors.blueAccent),
-          ],
-        ),
+        child: Text('Selected index: ${widget.currentIndex}'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: widget.currentIndex, // Use the passed currentIndex
+        onTap: widget.onTap, // Use the passed onTap function
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: 'Sell',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
