e61d569711916257e4e521134bf4a8cd30b15e25
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-28 10:36:56 +0100

    modifies the routes to include the onboarding screen and the initial screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,18 +1,17 @@
-// =================== lib/routes.dart ===================
-
+import 'package:car_plaza/screens/auth/login_screen.dart';
+import 'package:car_plaza/screens/auth/register_screen.dart';
+import 'package:car_plaza/screens/messages/messages_screen.dart';
+import 'package:car_plaza/screens/profile/profile_screen.dart';
+import 'package:car_plaza/screens/sell/sell_screen.dart';
 import 'package:flutter/material.dart';
-import 'screens/home/home_screen.dart';
-import 'screens/auth/login_screen.dart';
-import 'screens/sell/sell_screen.dart';
-import 'screens/messages/messages_screen.dart';
-import 'screens/profile/profile_screen.dart';
+
+import 'screens/onboarding/onboarding_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
-  '/': (context) => HomeScreen(),
-  '/login': (context) => LoginScreen(),
-  '/sell': (context) => SellScreen(),
-  '/messages': (context) => MessagesScreen(),
-  '/profile': (context) => ProfileScreen(),
+  '/': (context) => const OnboardingScreen(), // Default first screen now
+  '/login': (context) => const LoginScreen(),
+  '/register': (context) => const RegisterScreen(),
+  '/sell': (context) => const SellScreen(),
+  '/messages': (context) => const MessagesScreen(),
+  '/profile': (context) => const ProfileScreen(),
 };
-
-// ======================================================
