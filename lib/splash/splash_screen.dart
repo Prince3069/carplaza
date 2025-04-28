aab1e27ca38d3df5522d44294a16d57a020f4846
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-28 09:34:54 +0100

    added splash screen page with code

---
+++ lib/splash/splash_screen.dart
@@ -0,0 +1,54 @@
+import 'package:car_plaza/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:car_plaza/main.dart'; // To navigate to BottomNav after login
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigate();
+  }
+
+  void _navigate() async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const BottomNavScreen()),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png', height: 120),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(color: Colors.blueAccent),
+          ],
+        ),
+      ),
+    );
+  }
+}
