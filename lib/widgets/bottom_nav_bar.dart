fc76cd53e2b154f62fdbf2487a0ce853cf14d18d
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-28 13:38:27 +0100

    added bottom nav bar

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,23 +1,24 @@
-// =================== lib/widgets/bottom_nav_bar.dart ===================
-
 import 'package:flutter/material.dart';
 
 class BottomNavBar extends StatelessWidget {
   final int currentIndex;
   final Function(int) onTap;
 
-  const BottomNavBar(
-      {Key? key, required this.currentIndex, required this.onTap})
-      : super(key: key);
+  const BottomNavBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed, // Required for more than 3 items
       currentIndex: currentIndex,
       onTap: onTap,
       selectedItemColor: Colors.blue,
       unselectedItemColor: Colors.grey,
-      items: const [
+      items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
           label: 'Home',
@@ -27,20 +28,18 @@ class BottomNavBar extends StatelessWidget {
           label: 'Search',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.add_box),
+          icon: Icon(Icons.add_circle_outline),
           label: 'Sell',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.message),
+          icon: Icon(Icons.message_outlined),
           label: 'Messages',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person),
+          icon: Icon(Icons.person_outline),
           label: 'Profile',
         ),
       ],
     );
   }
 }
-
-// =====================================================================
