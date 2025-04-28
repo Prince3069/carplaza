f5ed6f147f7542301287a3552aef800e81f62e46
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-28 13:36:11 +0100

    updated routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,16 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:car_plaza/screens/auth/login_screen.dart';
 import 'package:car_plaza/screens/auth/register_screen.dart';
+import 'package:car_plaza/screens/auth/forgot_password_screen.dart';
+import 'package:car_plaza/screens/home/home_screen.dart';
 import 'package:car_plaza/screens/messages/messages_screen.dart';
 import 'package:car_plaza/screens/profile/profile_screen.dart';
 import 'package:car_plaza/screens/sell/sell_screen.dart';
-import 'package:flutter/material.dart';
-
-import 'screens/onboarding/onboarding_screen.dart';
+import 'package:car_plaza/screens/onboarding/onboarding_screen.dart';
+import 'package:car_plaza/screens/search_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
-  '/': (context) => const OnboardingScreen(), // Default first screen now
+  '/': (context) => const OnboardingScreen(), // Default first screen
   '/login': (context) => const LoginScreen(),
   '/register': (context) => const RegisterScreen(),
+  '/forgot_password': (context) => const ForgotPasswordScreen(),
+  '/home': (context) => const HomeScreen(), // Add home route
+  '/search': (context) => const SearchScreen(),
   '/sell': (context) => const SellScreen(),
   '/messages': (context) => const MessagesScreen(),
   '/profile': (context) => const ProfileScreen(),
