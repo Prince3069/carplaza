6faab8c144395e1b82067208bbd023eb8bd6531b
Prince3069 <Princenwanozie6666@gmail.com>
2025-05-03 18:45:00 +0100

    added reponsive layouts

---
+++ lib/utils/responsive.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Responsive {
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 600;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 600 &&
+      MediaQuery.of(context).size.width < 1200;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1200;
+
+  static double responsiveValue(
+    BuildContext context, {
+    required double mobile,
+    double? tablet,
+    double? desktop,
+  }) {
+    if (isDesktop(context)) return desktop ?? tablet ?? mobile;
+    if (isTablet(context)) return tablet ?? mobile;
+    return mobile;
+  }
+
+  static int responsiveGridCount(BuildContext context) {
+    if (isDesktop(context)) return 4;
+    if (isTablet(context)) return 3;
+    return 2;
+  }
+}
