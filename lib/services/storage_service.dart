d0e4df6a6fc2958dd8702afc0a2c369e85f798e8
Prince3069 <Princenwanozie6666@gmail.com>
2025-05-05 15:24:59 +0100

    added storage service

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,3 +1,48 @@
+// import 'package:firebase_storage/firebase_storage.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:car_plaza/utils/constants.dart';
+// import 'dart:io';
+
+// class StorageService {
+//   final FirebaseStorage _storage = FirebaseStorage.instance;
+
+//   Future<String> uploadProfileImage(String userId, XFile image) async {
+//     try {
+//       final ref = _storage.ref('${AppConstants.profileImagesPath}/$userId');
+//       final uploadTask = await ref.putFile(File(image.path));
+//       final downloadUrl = await uploadTask.ref.getDownloadURL();
+//       return downloadUrl;
+//     } catch (e) {
+//       throw Exception('Failed to upload profile image: $e');
+//     }
+//   }
+
+//   Future<List<String>> uploadCarImages(String carId, List<XFile> images) async {
+//     try {
+//       List<String> downloadUrls = [];
+//       for (int i = 0; i < images.length; i++) {
+//         final ref = _storage.ref('${AppConstants.carImagesPath}/$carId/$i');
+//         final uploadTask = await ref.putFile(File(images[i].path));
+//         final downloadUrl = await uploadTask.ref.getDownloadURL();
+//         downloadUrls.add(downloadUrl);
+//       }
+//       return downloadUrls;
+//     } catch (e) {
+//       throw Exception('Failed to upload car images: $e');
+//     }
+//   }
+
+//   Future<void> deleteCarImages(String carId) async {
+//     try {
+//       final ref = _storage.ref('${AppConstants.carImagesPath}/$carId');
+//       final listResult = await ref.listAll();
+//       await Future.wait(listResult.items.map((item) => item.delete()));
+//     } catch (e) {
+//       throw Exception('Failed to delete car images: $e');
+//     }
+//   }
+// }
+
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:car_plaza/utils/constants.dart';
@@ -10,8 +55,7 @@ class StorageService {
     try {
       final ref = _storage.ref('${AppConstants.profileImagesPath}/$userId');
       final uploadTask = await ref.putFile(File(image.path));
-      final downloadUrl = await uploadTask.ref.getDownloadURL();
-      return downloadUrl;
+      return await uploadTask.ref.getDownloadURL();
     } catch (e) {
       throw Exception('Failed to upload profile image: $e');
     }
@@ -23,8 +67,7 @@ class StorageService {
       for (int i = 0; i < images.length; i++) {
         final ref = _storage.ref('${AppConstants.carImagesPath}/$carId/$i');
         final uploadTask = await ref.putFile(File(images[i].path));
-        final downloadUrl = await uploadTask.ref.getDownloadURL();
-        downloadUrls.add(downloadUrl);
+        downloadUrls.add(await uploadTask.ref.getDownloadURL());
       }
       return downloadUrls;
     } catch (e) {
@@ -39,6 +82,14 @@ class StorageService {
       await Future.wait(listResult.items.map((item) => item.delete()));
     } catch (e) {
       throw Exception('Failed to delete car images: $e');
+    }
+  }
+
+  Future<String> getImageDownloadUrl(String path) async {
+    try {
+      return await _storage.ref(path).getDownloadURL();
+    } catch (e) {
+      throw Exception('Failed to get download URL: $e');
     }
   }
 }
