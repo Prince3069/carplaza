21190051ffe4a7230d0317dde38b4923bc94a266
Prince3069 <Princenwanozie6666@gmail.com>
2025-05-05 14:21:22 +0100

    added the car provider and make sure its working well

--- lib/providers/car_provider.dart
+++ lib/providers/car_provider.dart
@@ -1,1 +1,40 @@
+import 'package:car_plaza/models/car_model.dart';
+import 'package:car_plaza/services/firestore_service.dart';
+import 'package:flutter/material.dart';
 
+class CarProvider with ChangeNotifier {
+  final FirestoreService _firestoreService;
+  List<CarModel> _featuredCars = [];
+  List<CarModel> _latestCars = [];
+  bool _isLoading = false;
+
+  CarProvider(this._firestoreService);
+
+  List<CarModel> get featuredCars => _featuredCars;
+  List<CarModel> get latestCars => _latestCars;
+  bool get isLoading => _isLoading;
+
+  Future<void> loadFeaturedCars() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _featuredCars = await _firestoreService.getFeaturedCars().first;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> loadLatestCars() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _latestCars = await _firestoreService.getLatestCars().first;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
