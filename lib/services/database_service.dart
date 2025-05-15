20722131f1bab26a51b2aa00cbd6be0cd839c965
Prince3069 <Princenwanozie6666@gmail.com>
2025-05-15 01:06:32 +0100

    good to go remaining testing

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:car_plaza/models/car_model.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@ void main() {
   testWidgets('App starts and shows onboarding screen',
       (WidgetTester tester) async {
     // Build the app
-    await tester.pumpWidget(const CarPlazaApp());
+    await tester.pumpWidget(const MyApp());
 
     // Wait for animations/frame builds
     await tester.pumpAndSettle();
